ab8d1f6af8b51193389c04b56b828726f37c5354
Martin Altenstedt <Martin.Altenstedt@gmail.com>
2019-12-30 18:49:01 +0100

    Write measurement to file
    This commit will write the idle time to a file using protobuf format.

--- main.c
+++ main.c
@@ -1,10 +1,15 @@
 #include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <sys/types.h>
 #include <unistd.h>
 #include <X11/extensions/scrnsaver.h>
 
+#include "measurement.pb-c.h"
+
 // https://stackoverflow.com/a/4702411
-int get_idle_time() {
+long get_idle_time() {
   time_t idle_time;
   static XScreenSaverInfo *mit_info;
   Display *display;
@@ -30,6 +35,50 @@ int get_idle_time() {
 
 int main()
 {
-  printf("%d\n", get_idle_time());
+  char* home = getenv("HOME");
+
+  char topFolderName[1024];
+  snprintf(topFolderName, 1024, "%s/.flextime", home);
+
+  char dataFolderName[1024];
+  snprintf(dataFolderName, 1024, "%s/.flextime/data", home);
+
+  if (0 == mkdir(topFolderName, 0777)) {
+    mkdir(dataFolderName, 0777);
+    printf("Created folder %s\n", dataFolderName);
+  }
+
+  time_t now = time(NULL);
+
+  long idle = get_idle_time();
+
+  Measurement msg = MEASUREMENT__INIT;
+  void *buf;
+  unsigned len;
+
+  msg.kind = MEASUREMENT__KIND__MEASUREMENT;
+  msg.idle = idle;
+  msg.timestamp = (intmax_t)now;
+
+  len = measurement__get_packed_size(&msg);
+
+  buf = malloc(len);
+  measurement__pack(&msg,buf);
+
+  char formatTimeBuffer[20];
+  strftime(formatTimeBuffer, 20, "%FT%TZ", localtime(&now));
+
+  char fileName[1024];
+  snprintf(fileName, 1024, "%s/.flextime/data/%s.bin", home, formatTimeBuffer);
+  FILE *file = fopen(fileName, "w+");
+
+  printf("Writing %d bytes of idle %ld to %s\n", len, idle, fileName);
+
+  fwrite(buf, len, 1, file);
+
+  fclose(file);
+
+  free(buf);
+
   return 0;
 }
